ae39672c16e32783a849ffcd777fa31a8953d026
Aliya Hameer <adhameer@gmail.com>
2016-03-15 16:27:01 -0700

    Style fixes and comments for beehive.py.

--- beehive.py
+++ beehive.py
@@ -3,8 +3,8 @@
 A script to create a mininet topology with a given number of hosts
 connected by one switch, and run a beehive cluster on them.
 
-Put this script in your ~/mininet folder or give this sciprt the path
-to mininet folder(otherwise it won't be able to import from the
+Put this script in your ~/mininet folder or give this script the path
+to mininet folder (otherwise it won't be able to import from the
 mininet Python API). Run it as follows:
 
 sudo -E python beehive.py
@@ -39,107 +39,127 @@ CONTROLLER_HOST_NAME = "hcon"
 DEFAULT_PORT = 7677
 GO_RUN_ARGS = "-instrument"
 
+
 class TestTopo(Topo):
     """n hosts connected by a single switch."""
 
     def build(self, n=2):
+        """Set up n hives with one controller host."""
 
-        # There will be n beehive hosts with 1 controller host
-        # CPU limit of a host
-        cpu_imit = .5 / (n + 1)
+        # CPU limit of a non-controller host.
+        # The controller will get 50% CPU, and the remaining
+        # 50% will be split among the other hosts.
+        cpu_limit = .5 / (n + 1)
+
         switch = self.addSwitch("s1")
 
-        # Rhe controller host will get 50% CPU
-        #host = self.addHost(CONTROLLER_HOST_NAME, cpu=.5 )
-        host = self.addHost(CONTROLLER_HOST_NAME )
+        # The main controller host
+        host = self.addHost(CONTROLLER_HOST_NAME)
         self.addLink(host, switch)
 
+        # n hives connected to the controller
         for h in range(0, n):
-            host = self.addHost("h{}".format(h), cpu=cpu_imit)
+            host = self.addHost("h{}".format(h), cpu=cpu_limit)
             self.addLink(host, switch)
 
-def waitForHive(host_index):
-    print("Waiting hive {} hive to start ...".format(host_index))
-    path_to_hive_output = "beehiveOutput/{}.out".format(host_index)
+
+def wait_for_hive(hive_index):
+    """Busy-wait for the application on hive number hive_index to start up."""
+
+    print("Waiting for {} hive to start ...".format(hive_index))
+
+    path_to_hive_output = "beehiveOutput/{}.out".format(hive_index)
+
     # Hive is not started when there is no output file
     # or output file has no content
-    while (os.path.isfile(path_to_hive_output) == False \
-            or os.stat(path_to_hive_output).st_size == 0):
+    while (os.path.isfile(path_to_hive_output) == False or
+           os.stat(path_to_hive_output).st_size == 0):
         sleep(1)
-    print("Hive {} started".format(host_index))
 
-def getHostFullAddress(host, port = None):
-    if port == None:
+    print("Hive {} started".format(hive_index))
+
+
+def get_full_host_address(host, port=None):
+    """
+    Return the formatted IP address and port number of host.
+    host should be an instance of class Host.
+    If a port number is supplied, use that; otherwise, use the default
+    port number.
+    """
+
+    if port is None:
         port = DEFAULT_PORT
+
     return "{}:{}".format(host.IP(), port)
 
-def getRunCommand(host, id, application_path, peer_list = None):
-    go_run_app = "go run %s" % (application_path)
-    host_address = "-addr %s" % (getHostFullAddress(host))
+
+def getRunCommand(host, id, application_path, peer_list=None):
+    """
+    Return a command string that, when executed by host, starts up the
+    Beehive application located at application_path.
+    id is a unique identifier forhost.
+    """
+
+    go_run_app = "go run {}".format(application_path)
+    host_address = "-addr {}".format(get_full_host_address(host))
+
+    # Construct a list of peer addresses for the application
     peer_addresses = ""
-    if(peer_list != None):
-        peer_address_list = []
-        for peer in peer_list:
-            peer_address_list.append(getHostFullAddress(peer))
-        peer_addresses = "-paddrs %s" % (' '.join(peer_address_list))
+    if peer_list is not None:
+        peer_addresses = "-paddrs {}".format(
+            " ".join(get_full_host_address(peer) for peer in peer_list))
+
     state_path = "-statepath /tmp/beehive{}".format(id)
-    stdout_output = ">beehiveOutput/%s.out" % (id)
-    stderror_output = "2>beehiveOutput/%s.error.out" % (id)
-    command_parts = [go_run_app,
-                        host_address,
-                        peer_addresses,
-                        state_path,
-                        GO_RUN_ARGS,
-                        stdout_output,
-                        stderror_output,
-                        '&']
-    command = ' '.join(command_parts)
+    stdout_output = "> beehiveOutput/{}.out".format(id)
+    stderror_output = "2> beehiveOutput/{}.error.out".format(id)
 
-    return command
+    # Put all the command-line arguments together
+    return " ".join([
+        go_run_app,
+        host_address,
+        peer_addresses,
+        state_path,
+        GO_RUN_ARGS,
+        stdout_output,
+        stderror_output,
+        '&'])
 
-def runExperiment(num_hosts, application_path):
+
+def run_experiment(num_hosts, application_path):
     topo = TestTopo(n=num_hosts)
     net = Mininet(topo=topo, host=CPULimitedHost)
-    cmd_arg_string = "{}{}".format(
-        " " if GO_RUN_ARGS else "",
-        GO_RUN_ARGS)
     net.start()
     h0 = net.get("h0")
 
     # Start the initial end host that all peers will connect to
     command = getRunCommand(h0, 0, application_path)
-    h0.cmd("export PATH=$PATH:/usr/local/go/bin")
-    h0.cmd("export GOPATH=$HOME/work")
     print("Executing {} on host {}...".format(command, 0))
     h0.cmd(command)
-    waitForHive(0)
+    wait_for_hive(0)
 
     # Now start all the peers
     for i in range(1, num_hosts):
         host = net.get("h{}".format(i))
         command = getRunCommand(host, i, application_path, [h0])
-
-        host.cmd("export PATH=$PATH:/usr/local/go/bin")
-        host.cmd("export GOPATH=$HOME/work")
         print("Executing {} on host {}...".format(command, i))
         host.cmd(command)
 
-    # Wait for all hive to start
+    # Wait for all peers to start
     for i in range(1, num_hosts):
-        waitForHive(i)
+        wait_for_hive(i)
 
-    print("All hive started")
-    # print("Starting CLI, press CTRL-D or type 'exit' to exit.")
-    # CLI(net)
+    print("All hives started")
+
+    # Run the experiment and save the results to a file
     hcon = net.get("hcon")
     hcon.cmd("python ./metric.py >> experimentStdout 2>> experimentStderr")
     net.stop()
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: beehive.py "
-              + "[number-of-hosts]"
-              + "[path/to/application.go]")
+        print("Usage: beehive.py " +
+              "[number-of-hosts]" +
+              "[path/to/application.go]")
         sys.exit()
 
     try:
@@ -149,4 +169,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     setLogLevel("info")
-    runExperiment(num_hosts, sys.argv[2])
+    run_experiment(num_hosts, sys.argv[2])
